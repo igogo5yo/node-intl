ee166bcdfa8a65d2df91039dc15b2cd023e92a3d
Mykola Sedletskyi <icevita@gmail.com>
2017-09-14 15:03:09 +0300

    addMessageFromFile function implemented

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,5 +1,6 @@
 import NodeIntl from '../index';
 import { expect } from 'chai';
+import { join } from 'path';
 // if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
 // import 'mocha';
 
@@ -7,6 +8,7 @@ const mockMessages = {
     'test': 'Test'
 };
 const mockLocale = 'te-ST';
+const messagesFilePath = join(__dirname, 'te-ST.json');
 
 const intl = NodeIntl.instance;
 
@@ -14,7 +16,18 @@ describe('NodeIntl test', () => {
     it('should set locale', () => {
         expect(intl.locale).not.to.equal(mockLocale);
         intl.locale = mockLocale;
+        intl.messages = mockMessages;
         expect(intl.locale).to.equal(mockLocale);
+        expect(intl.messages).to.equal(intl.messages);
+    });
+
+    it('should change locale', () => {
+        intl.locale = mockLocale;
+        intl.messages = mockMessages;
+        expect(intl.locale).to.equal(mockLocale);
+        expect(intl.messages).to.equal(intl.messages);
+        intl.locale = 'new-Locale';
+        expect(intl.messages).to.deep.equal({});
     });
 
     it('should load messages', () => {
@@ -34,6 +47,17 @@ describe('NodeIntl test', () => {
             ...messages
         });
     });
+
+    it('should add messages from file', () => {
+        intl.messages = mockMessages;
+        intl.addMessagesFromFile(messagesFilePath);
+
+        expect(intl.messages).to.deep.equal({
+            ...mockMessages,
+            'test.string': 'Hi test'
+        });
+    });
+
 
     it('should format message', () => {
         expect(intl.formatMessage('test')).to.equal(mockMessages.test);
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,59 @@
 import IntlMessageFormat from 'intl-messageformat';
+import * as fs from 'fs';
 
 export default class NodeIntl {
     private _messages: object = {};
     private _locale: string = 'en-US';
     private static _instance: NodeIntl;
 
-    private constructor() {}
+    private constructor() {
+    }
 
     public static get instance() {
         return this._instance || (this._instance = new this());
     }
 
-    get messages():object {
+    get messages(): object {
         return this._messages;
     }
 
-    set messages(messages:object) {
+    set messages(messages: object) {
         this._messages = messages;
     }
 
-    get locale():string {
+    get locale(): string {
         return this._locale;
     }
 
-    set locale(locale:string) {
+    set locale(locale: string) {
+        if (this._locale !== locale) {
+            this._messages = {};
+        }
         this._locale = locale;
     }
 
-    addMessages(newMessages:object) {
+    addMessages(newMessages: object) {
         this.messages = {
             ...this.messages,
             ...newMessages
         };
     }
 
-    formatMessage(messageId:string, formats:object = {}) {
+    addMessagesFromFile(filePath: string) {
+        try {
+            const localeFile = fs.readFileSync(filePath);
+            try {
+                this.addMessages(JSON.parse(localeFile.toString()));
+            } catch (parseError) {
+                console.log(`unable to parse locales from file 
+                (maybe ${filePath} is empty or invalid json?): ${parseError}`);
+            }
+        } catch (readError) {
+            console.log(`Error reading ${filePath}:${readError}`);
+        }
+    }
+
+    formatMessage(messageId: string, formats: object = {}) {
         const message = new IntlMessageFormat(this.messages[messageId], this.locale);
         return message.format(formats);
     }
